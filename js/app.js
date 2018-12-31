3f9b8a501f2155b60bf84fb0ec60074eafa0387a
masatodejima <happyturn0413@gmail.com>
2018-12-31 23:05:28 +0900

    base function

--- js/app.js
+++ js/app.js
@@ -7,8 +7,72 @@ function flip() {
     $('.card').toggleClass('flipped');
 }
 
-const btn = document.querySelector('.button');
+var flashwords = [
+  {
+    ja:"猫",
+    en:"cat",
+  },
+  {
+    ja:"犬",
+    en:"dog",
+  },
+  {
+    ja:"蛸",
+    en:"octopus",
+  },
+  {
+    ja:"多才な",
+    en:"versatile",
+  },
+]
 
-btn.addEventListener('click', function(){
-  flip();
-});
+const english = document.querySelector('.front');
+const japanese = document.querySelector('.back');
+const btn = document.querySelector('.button');
+const message = document.querySelector('.message');
+var a = flashwords.length;
+
+function flash() {
+  while (a) {
+    var j = Math.floor( Math.random() * a );
+    var t = flashwords[--a];
+    flashwords[a] = flashwords[j];
+    flashwords[j] = t;
+  }
+  console.log(flashwords);
+}
+
+
+flash();
+
+var count = [];
+
+function doFlash(){
+
+  btn.addEventListener('click', function(){
+    flip();
+    var clicked = event.target;
+    count.push(clicked);
+    var countNum = count.length;
+
+    if (countNum%2==1){
+      japanese.innerHTML = flashwords[(countNum/2)+0.5].ja;
+    } else {
+      english.innerHTML = flashwords[countNum/2].en;
+    }
+
+    setTimeout(function(){
+      if (countNum%2==1){
+        message.innerHTML = "answer";
+      } else {
+        message.innerHTML = "next";
+      }
+    }, 500)
+
+
+
+
+  });
+};
+
+doFlash();
